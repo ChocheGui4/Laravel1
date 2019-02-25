67890826131e7e55efa138fdeca48ce78649c94f
Choche <chochej10@gmail.com>
2019-02-25 17:45:01 -0600

    Pruebas

---
+++ tests/Feature/UserModuleTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserModuleTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    function testExample()
+    {
+        $this->get('/usuarios')
+            ->assertStatus(200)
+            ->assertSee('Usuarios');
+    }
+    function Detalles()
+    {
+        $this->get('/usuarios/5')
+            ->assertStatus(200)
+            ->assertSee('Mostrar detalle del usuario: 5');
+    }
+    function Apodo()
+    {
+        $this->get('/saludo/Jose/Choche')
+            ->assertStatus(200)
+            ->assertSee('Bienvenido Jose, tu apodo es Choche');
+    }
+    function Nuevo()
+    {
+        $this->get('/usuarios/nuevo')
+            ->assertStatus(200)
+            ->assertSee('Crear nuevo usuario');
+    }
+}
