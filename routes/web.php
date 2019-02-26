d6ab3a8322e7b859ca87aea1701da446d9511a7e
Choche <chochej10@gmail.com>
2019-02-25 18:15:15 -0600

    Controladores

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,11 @@ Route::get('/', function (){
     return 'Home';
 });
 
-Route::get('/usuarios', function (){
-    return 'Usuarios';
-});
+Route::get('/usuarios', 'UserController@index');
 
 
-Route::get('/usuarios/{id}', function ($id){
-    return "Mostrar detalle del usuario: {$id}";
-})->where('id','[0-9]+');
+Route::get('/usuarios/{id}','UserController@show')
+    ->where('id','[0-9]+');
 
 
 Route::get('/usuarios/nuevo', function (){
